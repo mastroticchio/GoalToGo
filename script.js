6d91d1f3c277524196bd65c9df60ae86360ae36e
Mario Tiriticco <mariooo.tiri@gmail.com>
2026-04-01 18:18:08 +0200

    aggiugo pagina registrazione

--- script.js
+++ script.js
@@ -12,8 +12,79 @@ function handleClick(action) {
     case 'inizia':
       window.location.href = "home_page.html";
       break;
+    case 'inizia-registrazione':
+      handleRegister();
+      break;
     default:
       console.log("Azione sconosciuta");
+  }
+}
+
+function handleRegister() {
+  const isGiocatore = document
+    .querySelector(".toggle-btn.active")
+    .innerText.includes("giocatore");
+
+  if (isGiocatore) {
+    const inputs = document.querySelectorAll("#form-giocatore input");
+
+    const email = inputs[0].value;
+    const username = inputs[1].value;
+    const password = inputs[2].value;
+    const confirm = inputs[3].value;
+
+    if (!email || !username || !password || !confirm) {
+      alert("Compila tutti i campi");
+      return;
+    }
+
+    if (password !== confirm) {
+      alert("Le password non coincidono");
+      return;
+    }
+
+    console.log("Registrazione giocatore:", { email, username, password });
+
+  } else {
+    const inputs = document.querySelectorAll("#form-campo input");
+
+    const email = inputs[0].value;
+    const nome = inputs[1].value;
+    const indirizzo = inputs[2].value;
+    const password = inputs[3].value;
+    const confirm = inputs[4].value;
+
+    if (!email || !nome || !indirizzo || !password || !confirm) {
+      alert("Compila tutti i campi");
+      return;
+    }
+
+    if (password !== confirm) {
+      alert("Le password non coincidono");
+      return;
+    }
+
+    console.log("Registrazione campo:", { email, nome, indirizzo, password });
+  }
+  window.location.href = "home_page.html";
+}
+
+function switchMode(mode) {
+  const giocatore = document.getElementById("form-giocatore");
+  const campo = document.getElementById("form-campo");
+
+  const buttons = document.querySelectorAll(".toggle-btn");
+
+  buttons.forEach(btn => btn.classList.remove("active"));
+
+  if (mode === "giocatore") {
+    giocatore.classList.add("active-form");
+    campo.classList.remove("active-form");
+    buttons[0].classList.add("active");
+  } else {
+    campo.classList.add("active-form");
+    giocatore.classList.remove("active-form");
+    buttons[1].classList.add("active");
   }
 }
 
